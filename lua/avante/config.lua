4213cc6cb52f11bc0e8829b391744bcd7cd700c0
Paco Marquez <fmarquezx7@gmail.com>
2025-01-20 11:34:20 -0600

    Update config.lua

--- lua/avante/config.lua
+++ lua/avante/config.lua
@@ -12,7 +12,7 @@ M._defaults = {
   --provider = "claude", -- Only recommend using Claude
   --auto_suggestions_provider = "claude",
   provider = 'ollama',
-  auto_suggestions_provider = 'ollama_deepseek',
+  auto_suggestions_provider = 'ollama',
   
   ---@alias Tokenizer "tiktoken" | "hf"
   -- Used for counting tokens and encoding text.
